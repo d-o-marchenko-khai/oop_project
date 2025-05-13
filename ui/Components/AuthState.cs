0f63f8f04e19999ffb770c034eb42c2d9b44f774
Dima Marchenko <marchenko.dmitriy.00@gmail.com>
2025-05-13 21:04:00 +0200

    Added home screen

--- ui/Components/AuthState.cs
+++ ui/Components/AuthState.cs
@@ -2,5 +2,9 @@ using oop_project;
 
 public static class AuthState
 {
-    public static RegisteredUser? CurrentUser { get; set; }
+    public static User CurrentUser { get; set; } = new Guest(
+        AdvertisementRepository.Instance,
+        ChatRepository.Instance,
+        RegisteredUserRepository.Instance
+    );
 } 
