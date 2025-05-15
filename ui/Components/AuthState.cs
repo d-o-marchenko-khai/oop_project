dbd9966931981efaa5da3d53590fc66d2a869b0c
Dima Marchenko <marchenko.dmitriy.00@gmail.com>
2025-05-15 21:57:19 +0200

    Ad management pages

--- ui/Components/AuthState.cs
+++ ui/Components/AuthState.cs
@@ -2,9 +2,10 @@ using oop_project;
 
 public static class AuthState
 {
-    public static User CurrentUser { get; set; } = new Guest(
-        AdvertisementRepository.Instance,
-        ChatRepository.Instance,
-        RegisteredUserRepository.Instance
-    );
+    // public static User CurrentUser { get; set; } = new Guest(
+    //     AdvertisementRepository.Instance,
+    //     ChatRepository.Instance,
+    //     RegisteredUserRepository.Instance
+    // );
+    public static User CurrentUser { get; set; } = RegisteredUserRepository.Instance.GetAll().First();
 } 
